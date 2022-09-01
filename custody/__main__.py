b743bfd8a3754fe431eea3df8eed4c24235cbe92
Alisher A. Khassanov <a.khssnv@gmail.com>
2022-09-01 15:35:06 +0300

    Minimal FastAPI application

---
+++ custody/__main__.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import uvicorn
+
+
+web_app = FastAPI()
+
+
+@web_app.get("/")
+async def hello():
+    return {"hello": "world"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(web_app, host="0.0.0.0", port=8080)
