12a3e4e4f5135049a9c813fcec4195d3b1c5d84e
Alisher A. Khassanov <a.khssnv@gmail.com>
2022-09-08 03:17:44 +0300

    Specify root path for uvicorn

--- custody/__main__.py
+++ custody/__main__.py
@@ -1,5 +1,6 @@
 import uvicorn
+
 from custody.web import app
 
-if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, root_path="/custody")
