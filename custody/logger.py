4bd5598c460fcb6607734ab0414ed4846896e315
alex <alexander.smehnov@gmail.com>
2023-08-22 19:49:33 +0300

    Add loki logging

--- custody/logger.py
+++ custody/logger.py
@@ -5,7 +5,8 @@ logger = logging.getLogger("custodyLogger")
 
 logger.setLevel(logging.DEBUG)
 logFormatter = logging.Formatter(
-    "%(name)-12s %(asctime)s %(levelname)-8s %(filename)s:%(funcName)s %(message)s"
+    "%(name)-12s %(asctime)s %(levelname)-8s "
+    "%(filename)s:%(funcName)s %(action)s %(pack_uuid)s %(message)s"
 )
 consoleHandler = logging.StreamHandler(stdout)
 consoleHandler.setFormatter(logFormatter)
