5a3dd228163532e7efc759eb160a1d32ecdd49bb
alex <alexander.smehnov@gmail.com>
2023-08-25 05:21:44 +0300

    Format logs with json formatter

--- custody/logger.py
+++ custody/logger.py
@@ -1,14 +1,18 @@
 import logging
-from sys import stdout
+
+from pythonjsonlogger import jsonlogger
 
 logger = logging.getLogger("custodyLogger")
 
 logger.setLevel(logging.DEBUG)
-logFormatter = logging.Formatter(
-    "LOGGER_NAME=%(name)-12s ASCTIME=%(asctime)s LEVEL=%(levelname)-8s "
-    "SOURCE_FUNC=%(filename)s:%(funcName)s ACTION=%(action)s PACK_UUID=%(pack_uuid)s "
-    "MESSAGE=%(message)s"
-)
-consoleHandler = logging.StreamHandler(stdout)
-consoleHandler.setFormatter(logFormatter)
+
+# logFormatter = logging.Formatter(
+#     "LOGGER_NAME=%(name)-12s ASCTIME=%(asctime)s LEVEL=%(levelname)-8s "
+#     "SOURCE_FUNC=%(filename)s:%(funcName)s ACTION=%(action)s PACK_UUID=%(pack_uuid)s "
+#     "MESSAGE=%(message)s"
+# )
+formatter = jsonlogger.JsonFormatter()
+
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(formatter)
 logger.addHandler(consoleHandler)
